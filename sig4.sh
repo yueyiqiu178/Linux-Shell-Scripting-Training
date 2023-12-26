b86579b5c79c432af648c4cc1e7c6ad07600da09
MaxQiu <yueyiqiu178@gmail.com>
2023-12-26 18:01:52 +0800

    add sig

---
+++ sig4.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Removing a set trap
+#
+trap "echo ' Sorry... Ctrl-C is trapped.'" SIGINT
+#
+count=1
+while [ $count -le 5 ]
+do
+echo "Loop #$count"
+sleep 1
+count=$[ $count + 1 ]
+done
+#
+# Rmove the trap
+echo "I just removed the trap"
+trap - SIGINT
+#
+count=1
+while [ $count -le 5 ]
+do
+echo "Second Loop #$count"
+sleep 1
+count=$[ $count + 1 ]
+done
